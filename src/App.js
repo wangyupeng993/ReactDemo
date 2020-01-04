75bf59dc9ab16d5946c883e74d9688eef4f82c35
universe-peng <3464842637@qq.com>
2020-01-04 15:31:05 +0800

    react demo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './assets/css/icon.css';
+import './assets/css/main.css';
+import './assets/css/public.css';
+import './assets/css/style.css';
+import './assets/css/transition.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-main"><i className="cuIcon-mobilefill"></i></div>
+    );
+  }
+}
+
+export default App;
